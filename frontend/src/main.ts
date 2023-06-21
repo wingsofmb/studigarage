715c3b215c574b53cf5227fdba0cf6eb4a5fd492
samia Benjelloun <benjelloun.samia@gmail.com>
2023-06-21 09:11:30 +0200

    fix forward eslint config to proper repositories

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -4,8 +4,7 @@ import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter([]), 
-  provideAnimations()],
+  providers: [provideRouter([]), provideAnimations()],
 }).catch((e) => {
   console.error(e);
 });
