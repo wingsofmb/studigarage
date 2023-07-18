e5a3dee96ed98bc718fadc2ee3a3e80390e6558f
samia Benjelloun <benjelloun.samia@gmail.com>
2023-07-18 20:03:09 +0200

    rename BACKEND_PORT to PORT

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,6 @@ import { env } from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(env.BACKEND_PORT);
+  await app.listen(env.PORT);
 }
 bootstrap();
