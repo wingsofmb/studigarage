27e24dd806fff036345dddc971a8aea0a97cd22c
samia Benjelloun <benjelloun.samia@gmail.com>
2023-07-18 19:57:10 +0200

    define backend run port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from 'src/app.module';
+import { env } from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(env.BACKEND_PORT);
 }
 bootstrap();
